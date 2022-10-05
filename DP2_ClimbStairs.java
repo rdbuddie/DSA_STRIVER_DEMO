9da844862a34d00849a938ac04928e7ea5c36061
Rupankar Das <88283289+rupnkrdas@users.noreply.github.com>
2022-10-05 11:19:51 +0530

    Update DP2_ClimbStairs.java

--- DP2_ClimbStairs.java
+++ DP2_ClimbStairs.java
@@ -21,7 +21,7 @@ public class DP2_ClimbStairs {
     // Recursion
     public static int climbStairsRecursion(int n) {
         if (n == 0 || n == 1)
-            return 1;
+            return 1; // only one possible way
 
         int singleJump = climbStairsRecursion(n - 1);
         int doubleJump = climbStairsRecursion(n - 2);
